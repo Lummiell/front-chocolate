a086b78d6a97741d8588715075a39c56e228ebd8
Gabriel Valeriano <gabrielvaleriano18@outlook.com>
2020-05-08 23:11:32 -0300

    Ícones

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,23 +1,46 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+import {
+  FiUser,
+  FiLock,
+  FiArrowRightCircle,
+  FiChevronRight,
+  FiLogIn,
+  FiPlusSquare,
+} from "react-icons/fi";
 function Login() {
   const [usuario, setUsuario] = useState();
   const [Senha, setSenha] = useState();
+  const [erro, setErro] = useState("");
+  const Logar = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div id='Login'>
-      <div id='info'>
-    <h1>Amigo chocolate 🍫</h1>
-    <Link to="/NovoUsuario">
-                <button>Novo usuário</button>
-              </Link>
+    <div id="Login">
+      <div id="info">
+        <div id='TituloDesc'>
+          <h1>Amigo chocolate 🍫</h1>
+          <p>Pra você que é generoso ou não.</p>
+          <p>Faça sua conta já!</p>
+        </div>
+        <div id='Conta'>
+          <Link to="/NovoUsuario">
+            <button>
+              Novo usuário <FiPlusSquare />
+            </button>
+          </Link>
+        </div>
       </div>
       <div id="formLogin">
-        <form action="post">
+        <h2>Já tem uma conta? Faça login!</h2>
+        <form action="post" onSubmit={Logar}>
           <legend>Entre com os seus dados</legend>
           <ul>
             <li>
-              <label for="Usuario">Usuário </label>
+              <label for="Usuario">
+                <FiUser /> Usuário
+              </label>
             </li>
             <li>
               <input
@@ -28,7 +51,9 @@ function Login() {
               />
             </li>
             <li>
-              <label for="Senha">Senha </label>
+              <label for="Senha">
+                <FiLock /> Senha
+              </label>
             </li>
             <li>
               <input
@@ -39,10 +64,12 @@ function Login() {
               />
             </li>
             <li>
-              
               <button type="submit" id="enviar">
-                Login
+                Entrar <FiLogIn />
               </button>
+            </li>
+            <li>
+              <p id="Erro">{erro}</p>
             </li>
           </ul>
         </form>
